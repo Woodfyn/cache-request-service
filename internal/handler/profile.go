d96bc2c5da0a468555fab275bc85bc5d58b19cd3
Vova <woodfyn8020vo@gmail.com>
2024-07-03 17:21:01 +0300

    f

--- internal/handler/profile.go
+++ internal/handler/profile.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"context"
 	"errors"
+	"log/slog"
 
 	"github.com/Woodfyn/nexus-chat.cache-service/pkg/core"
 	cache_service "github.com/Woodfyn/nexus-chat.cache-service/pkg/proto"
@@ -10,6 +11,7 @@ import (
 )
 
 func (h *Handler) CreateProfile(ctx context.Context, req *cache_service.Request_Create_Profile) (*cache_service.Response_Empty, error) {
+	slog.Info("Take request", "request", req)
 	empty, err := h.cacheService.CreateProfile(ctx, req)
 	if err != nil {
 		return nil, NewErrorResponse(codes.Internal, err)
@@ -19,6 +21,7 @@ func (h *Handler) CreateProfile(ctx context.Context, req *cache_service.Request_
 }
 
 func (h *Handler) GetProfile(ctx context.Context, req *cache_service.Request_Key) (*cache_service.Response_Get_Profile, error) {
+	slog.Info("Take request", "request", req)
 	resp, err := h.cacheService.GetProfile(ctx, req)
 	if errors.Is(err, core.ErrCacheIsExpiredOrNotFound) {
 		return nil, NewErrorResponse(codes.NotFound, err)
@@ -30,6 +33,7 @@ func (h *Handler) GetProfile(ctx context.Context, req *cache_service.Request_Key
 }
 
 func (h *Handler) UpdateProfile(ctx context.Context, req *cache_service.Request_Update_Profile) (*cache_service.Response_Empty, error) {
+	slog.Info("Take request", "request", req)
 	empty, err := h.cacheService.UpdateProfile(ctx, req)
 	if errors.Is(err, core.ErrCacheIsExpiredOrNotFound) {
 		return nil, NewErrorResponse(codes.NotFound, err)
